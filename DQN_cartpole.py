a5eb1c42d840cce0ddb52736b0b78380c93059db
SeunghunJeon <kaist_jsh@kaist.ac.kr>
2021-10-22 18:51:19 +0900

    add Reference DQN learning tutorial using pytorch

--- DQN_cartpole.py
+++ DQN_cartpole.py
@@ -90,3 +90,7 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+## Reference ##
+## https://velog.io/@wjleekr927/%EA%B0%95%ED%99%94-%ED%95%99%EC%8A%B5-DQN-%ED%8A%9C%ED%86%A0%EB%A6%AC%EC%96%BC-PyTorch ##
